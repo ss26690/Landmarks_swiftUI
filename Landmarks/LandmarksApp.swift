ded8272f4296afe3b3cdd17bb1ed0f21bdb82cc6
Saurav Sagar <sagarsaurav1997@gmail.com>
2022-03-19 19:55:31 +0530

    Building Lists and Navigation

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Saurav Sagar on 18/03/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
